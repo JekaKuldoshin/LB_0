332b75f2b299a41d08548f944ade1ab713642a7d
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-09 11:53:18 +0300

    Переменные

---
+++ Prroject__1/Prroject__1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a1, b1, c1; //Эта пременная используеться для целых числовых операций
+	a1 = 22;
+	b1 = 45;
+	c1 = 32;
+
+	cout << "\t\t\t\t" << a1 << "\n\n";
+	cout << "\t\t\t\t" << b1 << "\n\n";
+	cout << "\t\t\t\t" << c1 << "\n\n";
+
+	double a2, b2, c2; //Эта пременная используеться для вывода не целых чисел (1.5)
+	a2 = 1.5;
+	b2 = 2.55;
+	c2 = 87.54;
+	cout << "\t\t" << a2 << "\n\n";
+	cout << "\t\t" << b2 << "\n\n";
+	cout << "\t\t" << c2 << "\n\n";
+
+	bool a3, b3, c3; //Эта пременная отвечает за логические операции (True or False)
+
+	a3 = false;
+	b3 = true;
+	c3 = false;
+
+	cout << "\t\t\t\t\t\t\t\t\t" << a3 << endl<<endl;
+	cout << "\t\t\t\t\t\t\t\t\t" << b3 << endl<<endl;
+	cout << "\t\t\t\t\t\t\t\t\t" << c3 <<  endl<<endl<<endl;
+
+
+	char a, b, c; // Это переменная используеться для символов.
+	a = 'H';
+	b = 'W';
+	c = 'W';
+
+
+	cout << "\t\t\t\t\t\t" << c << " \n\n";
+	cout <<"\t\t\t\t\t\t" << a << " \n\n";
+	cout << "\t\t\t\t\t\t" << b <<" \n\n"<< endl;
+}
