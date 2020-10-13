f3e2cd617e7d339cbcbaf12c56769c9fe5cca131
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-13 19:32:57 +0300

    Выучил cin

---
+++ Project__4/Project__4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Наш калькулятор еще в разработке." << endl;
+	cout << "Для вас пока мы можем предоставить возможность использовать сложение." << endl<<endl;
+	cout << "Введите число а: " << endl;
+
+	long long int a, b, Result;
+	
+	cin >> a;
+	cout << "Введите число b: " << endl;
+
+
+	cin >> b;
+	
+
+	Result = a + b;
+
+	cout << "Результат операции: " <<Result<< endl;
+
+
+}
