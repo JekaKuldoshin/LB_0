81cbb09e7833f77f7e7450eded46847d3d2a0bc0
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-13 18:33:03 +0300

    Выучил константы

---
+++ Project__2/Project__2.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	const int COUNT_DAYS_IN_WEEK = 7;
+
---
+++ Project__3/Project__3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+
+{
+	setlocale(LC_ALL, "Rus");
+
+	const int COUNT_DAYS_IN_WEEK = 7; //это не изменное число
+
+	const char NEW_LINE = '\n'; //Можна также использовать как для переноса и для табуляции (пробела)
+
+	const char TAB = '\t';
+
+	cout <<TAB<<TAB<< COUNT_DAYS_IN_WEEK << NEW_LINE;
+ }
