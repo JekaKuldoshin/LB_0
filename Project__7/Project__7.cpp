f6ccba20a2ab79c68f754b0da93dfeb2ae236378
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-14 09:39:55 +0300

    Формулы сокращения

---
+++ Project__7/Project__7.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "Rus");
+
+	int a = 0;
+	cout << a << endl;
+
+	a = a + 20;
+	cout << a << endl;
+	a += 20;
+	cout << a << endl;
+
+	a -= 10;
+	cout << a << endl;
+
+	a *= 2;
+	cout << a << endl;
+
+	a /= 2;
+	cout << a << endl;
+}
