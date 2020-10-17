73e201c7eda31947303ed99a3c0419237fa2330e
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-17 14:56:21 +0300

    if or else if

---
+++ Project_10_DZ__If__Else/Project_10_DZ__If__Else.cpp
@@ -0,0 +1,32 @@
+﻿//Напишите программу, проверяющую число, введенное с клавиатуры на четность
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+	cout << "Программа которая проверяет на четность." << endl << endl;
+
+	int a;
+	cout << "Введите число:";
+	cin >> a; 
+	cout<<endl;
+
+	if (a%2==0)
+	{
+		cout<<"Ваше число четное" << endl;
+	}
+
+	else
+	{
+		cout << "Ваше число не четное" << endl;
+	}
+
+
+
+
+
+}
