6d7967204e59065d996c98694b2242c513bd6f78
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-17 15:07:19 +0300

    Четное, не четное

---
+++ Project_11/Project_11.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int var;
+	cout << "Введите число для проверки на четность: ";
+	cin >> var;
+
+	if(var%2, var==0)
+	{
+		cout << "Число " <<var << " четное!"<<endl;
+	}
+
+	else
+	{
+		cout << "Число " << var << " не четное!" << endl;
+	}
+
+}
