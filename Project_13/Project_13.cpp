bb8f0447653deabb64dd693a9da39f7a3403d129
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-19 18:55:44 +0300

    While - цикл

---
+++ Project_13/Project_13.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int a=10;
+
+	while (a < 20)
+	{
+		cout << "Переменная a= " <<a<< endl;
+		a++;
+
+	}
+}
