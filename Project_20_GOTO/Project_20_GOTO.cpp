78bdf0671b71fb1b6af298481f92c4cef868fb14
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-27 11:47:26 +0200

    ---

---
+++ Project_20_GOTO/Project_20_GOTO.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	cout << "Один" << endl;
+
+	goto link;
+
+	cout << "Два" << endl;
+	cout << "Три" << endl;
+
+    link:
+
+	cout << "Четыре" << endl;
+	cout << "Пять" << endl;
+
+
+	system("pause");
+}
---
+++ Project_21_Отладчик/Project_21_Отладчик.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+
+int Sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int a = 5;
+
+	int b = 6;
+
+	///
+	///
+	///
+	///
+	///
+	///
+	///
+	///
+	///
+	int c;
+
+	c = a + b;
+
+	cout << c << endl;
+
+}
