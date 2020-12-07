f63eab3fb184e71d026ed2e7784521b1b814b185
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-12-07 11:23:53 +0200

    ++++

---
+++ Project_24_Массивы/Project_24_Массивы/Project_24_Массивы.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	/*srand(time(NULL));*/
+    const int size = 10;
+
+	int arr[size]{55,34,6,34,743,76,432,653,543,1};
+
+	for (int i = 0; i < size; i++)
+	{
+
+		arr[i] = i;
+	}
+
+
+
+
+	for (int i = 0; i < size; i++)
+	{
+		
+		cout << arr[i] << endl;
+	}
+
+
+
+
+
+
+}
