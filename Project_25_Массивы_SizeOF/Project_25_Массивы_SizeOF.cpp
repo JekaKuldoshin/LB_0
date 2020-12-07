13be664a61118f1f316f59f336f17e73471d8c0e
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-12-07 11:52:52 +0200

    +++

---
+++ Project_25_Массивы_SizeOF/Project_25_Массивы_SizeOF.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	
+	double arr[]{4,33,2523,643,3324};
+
+	/*int a = sizeof(arr);
+	int b = sizeof(int);
+
+
+	cout << sizeof(arr)/sizeof(int) << endl;*/
+
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+
+
+
+}
