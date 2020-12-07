a93e7d53b34a3baa53a4c413a7cf00dffe5a4110
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-12-07 12:16:14 +0200

    +++

---
+++ Project_26_Массив_Rand_Srand/Project_26_Массив_Rand_Srand.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	srand(time(NULL));
+
+	/*int a = rand() % 10 + 5;
+	cout << a << endl;
+
+	 a = rand() % 10 + 5;
+	cout << a << endl;*/
+
+
+	int const SIZE = 10;
+	int arr[SIZE];
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 100;
+
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+}
