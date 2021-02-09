70735315c8939a0efee7ea1a11bfea9ed2e43450
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-02-09 10:08:50 +0200

    +++

---
+++ Project__28_Функция/Project__28_Функция.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void foo()
+{
+    cout << "Я функция, меня вызвали!!" << endl;
+}
+
+int Sum(int a, int b)
+{
+    /*int result;
+    result = a + b;*/
+
+    return a + b;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    foo();
+    cout << endl<<endl;
+
+    /*int c;*/
+    int q = 11;
+    int w = 12;
+   /* c = Sum(q, w);*/
+
+    cout << Sum(q,w) << endl;
+}
+
