22d9ae0b019023cb1c55472876834ce2ceac7626
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-02-09 10:47:24 +0200

    +++

---
+++ Project__28_Функция/Функция__1/Функция__1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void FillArray(int arr[], const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10;
+    }
+}
+
+void PrintArray(int arr[], const int  size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    const int SIZE = 10;
+    int arr[SIZE];
+
+    FillArray(arr, SIZE);
+    PrintArray(arr, SIZE);
+}
+
---
+++ Project__28_Функция/Функция__2/Функция__2.cpp
@@ -0,0 +1,20 @@
+﻿//Прототипы функции
+
+#include <iostream>
+
+using namespace std;
+
+void Foo();
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    Foo();
+}
+
+
+void Foo()
+{
+    cout << "Я функция, меня Вывели!!" << endl;
+}
